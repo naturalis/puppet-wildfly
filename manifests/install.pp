19de9f0abe1dd66c54fb78d00d06771b96ea2af7
Atze de Vries <atze.devries@naturalis.nl>
2014-10-13 10:30:35 +0200

    a fix for install

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,41 +8,47 @@ class wildfly::install {
   if ($wildfly::use_web_download) {
     exec { 'download-wildfly':
       command => "/usr/bin/wget http://download.jboss.org/wildfly/${wildfly::version}/wildfly-${wildfly::version}.tar.gz",
-      cwd     => "/usr/src",
+      cwd     => '/usr/src',
       creates => "/usr/src/wildfly-${wildfly::version}.tar.gz",
+    }
+    exec { 'untar-wildfly':
+      command     => "tar xzf /usr/src/wildfly-${wildfly::version}.tar.gz",
+      cwd         => $wildfly::install_dir,
+      creates     => "${wildfly::install_dir}/wildfly-${wildfly::version}",
+      subscribe   => Exec['download-wildfly'],
+      refreshonly => true
     }
   }else{
     file { "/usr/src/wildfly-${wildfly::version}.tar.gz":
       source => [ "puppet:///modules/wildfly/wildfly-${wildfly::version}.tar.gz" ]
     }
-  }
-
-  exec { 'untar-wildfly':
-    command     => "tar xzf /usr/src/wildfly-${wildfly::version}.tar.gz",
-    cwd         => "${wildfly::install_dir}",
-    creates     => "${wildfly::install_dir}/wildfly-${wildfly::version}",
-    subscribe   => File["/usr/src/wildfly-${wildfly::version}.tar.gz"],
-    refreshonly => true
+    exec { 'untar-wildfly':
+      command     => "tar xzf /usr/src/wildfly-${wildfly::version}.tar.gz",
+      cwd         => $wildfly::install_dir,
+      creates     => "${wildfly::install_dir}/wildfly-${wildfly::version}",
+      subscribe   => File["/usr/src/wildfly-${wildfly::version}.tar.gz"],
+      refreshonly => true
+    }
   }
 
   file { "${wildfly::install_dir}/wildfly":
     ensure  => 'link',
-    owner   => "${wildfly::user}",
-    group   => "${wildfly::user}",
+    owner   => $wildfly::user,
+    group   => $wildfly::user,
     target  => "${wildfly::install_dir}/wildfly-${wildfly::version}",
     require => Exec[untar-wildfly]
   }
 
-  user { "${wildfly::user}":
-    shell   => "${wildfly::shell}",
-    ensure  => "present"
+  user { $wildfly::user :
+    ensure  => 'present',
+    shell   => $wildfly::shell,
   }
 
-  exec { chown-wildfly:
-    command => "chown -R ${wildfly::user}:${wildfly::user} ${wildfly::install_dir}/wildfly/*",
-    require => [ User["${wildfly::user}"], File["${wildfly::install_dir}/wildfly"] ],
-    subscribe => Exec[untar-wildfly],
-    refreshonly => true
+  exec { 'chown-wildfly' :
+    command     => "chown -R ${wildfly::user}:${wildfly::user} ${wildfly::install_dir}/wildfly/*",
+    require     => [ User[$wildfly::user], File["${wildfly::install_dir}/wildfly"] ],
+    subscribe   => Exec['untar-wildfly'],
+    refreshonly => true,
   }
 
 }
